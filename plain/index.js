4e4f772ef679fb014f0134c375b3a6ef981a0966
PlexSheep <PlexSheep@protonmail.com>
2022-11-25 11:24:35 +0100

    added some JS to plain

---
+++ plain/index.js
@@ -0,0 +1,8 @@
+function getTypes(){
+    var empty;
+    console.log("Here are some types of vars:");
+    console.log(typeof(1));
+    console.log(typeof("1"));
+    console.log(typeof(empty));
+    console.log(typeof(null));
+}
