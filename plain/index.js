fb55a088dda8431d16a8d8de0827152a6a1c55a0
PlexSheep <PlexSheep@protonmail.com>
2022-11-25 12:11:33 +0100

    added popup with half working JS

--- plain/index.js
+++ plain/index.js
@@ -6,3 +6,20 @@ function getTypes(){
     console.log(typeof(empty));
     console.log(typeof(null));
 }
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
+function processForm() {
+    console.log("processing");
+    const myForm = document.getElementById('myForm');
+    console.log(document.getElementById('myForm-name').value);
+    console.log(document.getElementById('myForm-age').value);
+
+    // now do something with those shitty vars.
+    closeForm();
+}
